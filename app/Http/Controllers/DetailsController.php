9b252656d28fd440204d3c47b9134f4e4c67bfa8
ljnnna <heymenania@gmail.com>
2025-06-17 12:49:15 +0700

    fixing error details

--- app/Http/Controllers/DetailsController.php
+++ app/Http/Controllers/DetailsController.php
@@ -1,13 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Product;
+use App\Models\ProductImage;
 use Illuminate\Http\Request;
 
 class DetailsController extends Controller
 {
-    public function details()
-    {
-        return view('pages.customer.detailsproductcatalogcust');
-    }
+    public function detailsById($id)
+{
+    $product = Product::with('images')->findOrFail($id);
+    $productImages = $product->images->pluck('path')->toArray();
+
+    return view('pages.customer.detailsproductcatalogcust', compact('product', 'productImages'));
+}
+
 }
